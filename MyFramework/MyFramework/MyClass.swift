322630655b9ccb8a7091199418cc16ebe5d02ae4
EgorMezhin <egorswege@yandex.com>
2021-06-11 11:34:45 +0300

    Add Framework

---
+++ MyFramework/MyFramework/MyClass.swift
@@ -0,0 +1,16 @@
+//
+//  MyClass.swift
+//  MyFramework
+//
+//  Created by Egor Lass on 11.06.2021.
+//
+
+
+public class MyClass {
+    
+    public let name = "Egor"
+    
+    public func sayHello() {
+        print("Hello!")
+    }
+}
